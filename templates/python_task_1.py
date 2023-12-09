91cddef47f012bc9965a077ea5846740663f27ab
MAHESH960 <56758337+MAHESH960@users.noreply.github.com>
2023-12-09 20:50:52 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -12,7 +12,22 @@ def generate_car_matrix(df)->pd.DataFrame:
         pandas.DataFrame: Matrix generated with 'car' values, 
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
-    # Write your logic here
+import pandas as pd
+
+# Assuming to have some data for 'car' values and corresponding 'id_1' and 'id_2'
+data = {'id_1': ['A', 'B', 'C'],
+        'id_2': ['X', 'Y', 'Z'],
+        'car': ['Toyota', 'Honda', 'Ford']}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+# Set 'id_1' and 'id_2' as indices and columns
+df.set_index(['id_1', 'id_2'], inplace=True)
+
+# Display the DataFrame
+print(df)
+
 
     return df
 
@@ -27,7 +42,22 @@ def get_type_count(df)->dict:
     Returns:
         dict: A dictionary with car types as keys and their counts as values.
     """
-    # Write your logic here
+    import pandas as pd
+
+def categorize_and_count_cars(df):
+    # Check if 'car' column exists in the DataFrame
+    if 'car' not in df.columns:
+        raise ValueError("DataFrame must contain a 'car' column")
+
+    # Group by 'car' and count occurrences
+    car_counts = df['car'].value_counts().to_dict()
+
+    return car_counts
+
+# Example usage:
+# Assuming df is the DataFrame you created earlier
+result = categorize_and_count_cars(df)
+print(result)
 
     return dict()
 
@@ -42,7 +72,25 @@ def get_bus_indexes(df)->list:
     Returns:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
-    # Write your logic here
+  import pandas as pd
+
+def indexes_where_bus_exceeds_twice_mean(df):
+    # Check if 'bus' column exists in the DataFrame
+    if 'bus' not in df.columns:
+        raise ValueError("DataFrame must contain a 'bus' column")
+
+    # Calculate the mean of 'bus' values
+    bus_mean = df['bus'].mean()
+
+    # Find indexes where 'bus' values exceed twice the mean
+    indexes = df.index[df['bus'] > 2 * bus_mean].tolist()
+
+    return indexes
+
+# Example usage:
+# Assuming df is the DataFrame you're working with
+result_indexes = indexes_where_bus_exceeds_twice_mean(df)
+print(result_indexes)
 
     return list()
 
@@ -57,7 +105,26 @@ def filter_routes(df)->list:
     Returns:
         list: List of route names with average 'truck' values greater than 7.
     """
-    # Write your logic here
+   import pandas as pd
+
+def routes_with_average_truck_greater_than_seven(df):
+    # Check if 'truck' column exists in the DataFrame
+    if 'truck' not in df.columns:
+        raise ValueError("DataFrame must contain a 'truck' column")
+
+    # Calculate the mean of 'truck' values for each route
+    route_means = df.groupby('route')['truck'].mean()
+
+    # Filter routes with average 'truck' values greater than 7
+    selected_routes = route_means[route_means > 7].index.tolist()
+
+    return selected_routes
+
+# Example usage:
+# Assuming df is the DataFrame you're working with
+result_routes = routes_with_average_truck_greater_than_seven(df)
+print(result_routes)
+
 
     return list()
 
@@ -72,7 +139,31 @@ def multiply_matrix(matrix)->pd.DataFrame:
     Returns:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
-    # Write your logic here
+ import pandas as pd
+
+def custom_multiplier(value):
+    # Define your custom conditions for multiplication
+    if value < 0:
+        return value * 2
+    elif value > 10:
+        return value * 1.5
+    else:
+        return value
+
+def multiply_matrix_with_conditions(matrix):
+    # Apply the custom_multiplier function to each element in the matrix
+    modified_matrix = matrix.applymap(custom_multiplier)
+
+    return modified_matrix
+
+# Example usage:
+# Assuming 'matrix' is your pandas DataFrame
+matrix_data = {'A': [5, -3, 12], 'B': [8, 4, 6], 'C': [15, 2, 9]}
+matrix = pd.DataFrame(matrix_data)
+
+# Multiply matrix values based on custom conditions
+result_matrix = multiply_matrix_with_conditions(matrix)
+print(result_matrix)
 
     return matrix
 
@@ -87,6 +178,29 @@ def time_check(df)->pd.Series:
     Returns:
         pd.Series: return a boolean series
     """
-    # Write your logic here
+import pandas as pd
+
+def check_completeness(df):
+    # Check if the DataFrame contains the required columns
+    required_columns = ['id', 'id_2', 'timestamp']
+    if not all(column in df.columns for column in required_columns):
+        raise ValueError("DataFrame must contain 'id', 'id_2', and 'timestamp' columns")
+
+    # Convert 'timestamp' to datetime
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+    # Calculate the time range for each ('id', 'id_2') pair
+    time_range = df.groupby(['id', 'id_2'])['timestamp'].agg(lambda x: (x.max() - x.min()))
+
+    # Check if the time range covers a full 24-hour and 7-day period
+    completeness_check = (time_range >= pd.Timedelta(days=7)) & (time_range >= pd.Timedelta(hours=24))
+
+    return completeness_check
+
+# Example usage:
+# Assuming df is your DataFrame
+completeness_result = check_completeness(df)
+print(completeness_result)
+
 
     return pd.Series()
